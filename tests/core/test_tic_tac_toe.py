c8f3e17a1a7c330c47b8c5bbb72dc9b317562447
dg-codes <dgcodes@outlook.com>
2024-02-14 17:08:46 +0000

    Add additional unit tests for GameBoard

--- tests/core/test_tic_tac_toe.py
+++ tests/core/test_tic_tac_toe.py
@@ -29,19 +29,37 @@ class TestBoardCell:
             cell = tic_tac_toe.BoardCell("A")
 
 
+@pytest.fixture
+def game_board() -> tic_tac_toe.GameBoard:
+    game_board = tic_tac_toe.GameBoard()
+    game_board._matrix = {
+        "A": tic_tac_toe.BoardCell("X"),
+        "B": tic_tac_toe.BoardCell("X"),
+        "C": tic_tac_toe.BoardCell("X"),
+        "D": tic_tac_toe.BoardCell("X"),
+        "E": tic_tac_toe.BoardCell("X"),
+        "F": tic_tac_toe.BoardCell("X"),
+        "G": tic_tac_toe.BoardCell("X"),
+        "H": tic_tac_toe.BoardCell("X"),
+        "I": tic_tac_toe.BoardCell("X"),
+    }
+    return game_board
+
+
 class TestGameBoard:
-    @classmethod
-    def test_is_full(cls):
-        board = tic_tac_toe.GameBoard()
-        board._matrix = {
-            "A": tic_tac_toe.BoardCell("X"),
-            "B": tic_tac_toe.BoardCell("X"),
-            "C": tic_tac_toe.BoardCell("X"),
-            "D": tic_tac_toe.BoardCell("X"),
-            "E": tic_tac_toe.BoardCell("X"),
-            "F": tic_tac_toe.BoardCell("X"),
-            "G": tic_tac_toe.BoardCell("X"),
-            "H": tic_tac_toe.BoardCell("X"),
-            "I": tic_tac_toe.BoardCell("X"),
-        }
-        assert board.is_full
+    def test_is_full(self, game_board: tic_tac_toe.GameBoard):
+        assert game_board.is_full
+
+    def test_board_should_not_update(self, game_board: tic_tac_toe.GameBoard):
+        game_board.update_board("A", "O")
+        _board_values = set(
+            [entry.value for entry in game_board._matrix.values()]
+        )
+        assert "O" not in _board_values
+
+    def test_board_should_update(self, game_board: tic_tac_toe.GameBoard):
+        game_board.update_board("A", "O", True)
+        _board_values = set(
+            [entry.value for entry in game_board._matrix.values()]
+        )
+        assert "O" in _board_values
